c176170f78ad601bf73457ce2f9a7251c95677fd
abrantesasf <abrantesasf@gmail.com>
2019-01-16 08:08:51 -0200

    Remoção dos diretórios antigos do LAFF. Criação de novo diretório.

--- linear_algebra/laff/LAFF-2.0xM/Answers/Week04/test_Trmv_ln_unb_var2.m
+++
@@ -1,9 +0,0 @@
-A = randi( [ -2, 2 ], 4, 4 )
-x = randi( [ -2, 2 ], 4, 1 )
-
-if ( isequal( Trmv_ln_unb_var2( A, x ), tril( A ) * x  ) )
-    disp( 'Trmv_ln_unb_var2 appears to be correct' )
-else
-    disp( 'Trmv_ln_unb_var2 has a problem' )
-end
-
